2b1313eca09d52fc076083b8298ab822e02b3c10
maciej88 <mkrywult88@gmail.com>
2024-02-17 12:33:49 +0100

    create account code refactor

--- src/z9_chargers/charger.py
+++ src/z9_chargers/charger.py
@@ -9,23 +9,22 @@ class ChargingSystem:
     def __init__(self):
         self.chargers: dict[Charger] = dict()
         self.__accounts: dict[ClientAccount] = dict()
-        self.cars: dict[Car] = dict()
+        self.cars = dict()
         self.password_hasher = PasswordHasher()
 
-    def create_account(self, name: str, funds: float, password: str):
+    def create_account(self, name: str, funds: float, password: str) -> ClientAccount:
         hashed_password = self.password_hasher.hash(password)
-        new_account = {
-            "id": uuid4(),
-            "name": name,
-            "funds": funds,
-            "password": hashed_password
-        }
-        self.__accounts[new_account["id"]] = new_account
+        new_account = ClientAccount(
+            id=uuid4(),
+            name=name,
+            funds=funds,
+            password=hashed_password
+        )
+        self.__accounts[new_account.id] = new_account
         return new_account
 
     def crete_cars(self, vin: str, max_charging_power: str) -> list[Car]:
         pass
-
 
 
 
